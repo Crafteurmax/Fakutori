7796737dae1a03b20880f43ccb75b8d8a8341e9a
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2024-10-27 19:21:10 +0100

    add kanji conversions and kanji datas

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -25,44 +25,52 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // start at an angle of around 45°
         angleHeight = Mathf.Atan2(cameraTransform.localPosition.y, -cameraTransform.localPosition.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // adjust the center point rotation and rotation according to input movement
         center.Translate(new Vector3(movement.x,0, movement.y)*speed*Time.deltaTime);
         center.Rotate(new Vector3(0,rotation.x * rotationSpeed * Time.deltaTime * 5.7f,0),Space.World);
 
+        // adjust the angle between the ground and the camera, clamp it on one quarter of a circle
         angleHeight -= rotation.y * rotationSpeed * Time.deltaTime * 0.1f;
         angleHeight = Mathf.Clamp(angleHeight, 0, 1.56f);
         cameraTransform.LookAt(center);
 
+        // set the zoom
         distanceToCenter += scroll * scrollSpeed * Time.deltaTime;
         distanceToCenter = Mathf.Clamp(distanceToCenter, 1, distanceMaxToCenter);
 
-
+        // set the position of the camera
         cameraTransform.localPosition = new Vector3(0, distanceToCenter*Mathf.Sin(angleHeight) , -distanceToCenter * Mathf.Cos(angleHeight));
 
     }
 
     public void Move(InputAction.CallbackContext context)
     {
+        // move event Handler
         movement = context.ReadValue<Vector2>();
     }
 
     public void Rotatae(InputAction.CallbackContext context)
     {
-        if(isInRotateMode) rotation = context.ReadValue<Vector2>();
+        // Rotate event Handler
+        if (isInRotateMode) rotation = context.ReadValue<Vector2>();
     }
 
     public void Zoom(InputAction.CallbackContext context)
     {
+        // Zoom event Handler
         scroll = context.ReadValue<float>() / 120f;
     }
 
     public void RotateMode(InputAction.CallbackContext context)
     {
+        // Rotation mode event Handler
         if (context.phase == InputActionPhase.Started)
         {
             isInRotateMode = true;
---
+++ Assets/Scripts/Library.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Library : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private string kanjiSaveFolder = Application.dataPath + "/RawData/vocab.csv";
+    private Dictionary<string, List<string>> hiraganaToKanjiDictionary = new Dictionary<string, List<string>>();
+    private Dictionary<string, List<string>> KanjiToHiraganaDictionary = new Dictionary<string, List<string>>();
+
+    void Start()
+    {
+        string rawData = System.IO.File.ReadAllText(kanjiSaveFolder);
+        string[] rawDataArray = rawData.Split(new string[] { ",", "\n" }, System.StringSplitOptions.None);
+
+        for (int i = 3; i < rawDataArray.Length; i += 3)
+        {
+            AddKanjiToHiraganaToKanjiDictionary(rawDataArray[i+1], rawDataArray[i]);
+            AddHiraganaToKanjiToHiraganaDictionary(rawDataArray[i], rawDataArray[i + 1]);
+        }
+        Debug.Log(GetKanjiFromKana("こう")[0]);
+        Debug.Log(GetKanaFromKanji("好")[0]);
+
+    }
+
+
+    public List<string> GetKanjiFromKana(string kana)
+    {
+        List<string> retour = new List<string>();
+        if (hiraganaToKanjiDictionary.TryGetValue(kana,out retour)) return retour;
+        return null;
+    }
+
+    private void AddKanjiToHiraganaToKanjiDictionary(string kana, string kanji)
+    {
+        if (hiraganaToKanjiDictionary.ContainsKey(kana)) hiraganaToKanjiDictionary[kana].Add(kanji);
+        else
+        {
+            List<string> list = new List<string>();
+            list.Add(kanji);
+            hiraganaToKanjiDictionary.Add(kana, list);
+        }
+    }
+
+    public List<string> GetKanaFromKanji(string kanji)
+    {
+        List<string> retour = new List<string>();
+        if (KanjiToHiraganaDictionary.TryGetValue(kanji, out retour)) return retour;
+        return null;
+    }
+
+    private void AddHiraganaToKanjiToHiraganaDictionary(string kanji, string kana)
+    {
+        if (KanjiToHiraganaDictionary.ContainsKey(kanji)) KanjiToHiraganaDictionary[kanji].Add(kana);
+        else
+        {
+            List<string> list = new List<string>();
+            list.Add(kana);
+            KanjiToHiraganaDictionary.Add(kanji, list);
+        }
+    }
+}
