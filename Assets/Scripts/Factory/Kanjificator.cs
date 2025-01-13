0dd39ad7af1ed0c4dfadc3c64f40bb414bfe6e68
X4ch <t.moilleron@gmail.com>
2025-01-13 14:52:09 +0100

    Kanji selection UI

--- Assets/Scripts/Factory/Kanjificator.cs
+++ Assets/Scripts/Factory/Kanjificator.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text.RegularExpressions;
+using UnityEngine.EventSystems;
+using JetBrains.Annotations;
 
 public class Kanjificator : Factory
 {
     [SerializeField] protected List<Item.Symbol> producedItemCharacters = new List<Item.Symbol>();
+    [SerializeField] private KanjificatorOutputSelection outputSelection;
+    private string stringOutput = string.Empty;
 
     public override void OnEnable()
     {
         base.OnEnable();
         SetBuildingType(BuildingType.Kanjificator);
+    }
+    private void Start()
+    {
+        outputSelection = FindAnyObjectByType<KanjificatorOutputSelection>();
     }
 
     public override IEnumerator ProduceItem()
@@ -24,11 +32,38 @@ public class Kanjificator : Factory
 
         if (!TryGetOutputsInCache(cachedInput, out characters))
         {
-            List<string> kanjiOuput;
-            if(Library.TryGetKanjiFromKana(inputedItem.ToString(), out kanjiOuput))
+            List<string> kanjiOuput = new List<string>();
+            if (Library.TryGetKanjiFromKana(inputedItem.ToString(), out kanjiOuput))
             {
                 // pour l'instant on prend que le premier mot que l'on trouve a voir si l'on a besoin d'autre chose plus tard
-                foreach(char c in kanjiOuput[0])
+                if (kanjiOuput.Count > 1)
+                {
+                    //Debug.Log("Number of choices: " + kanjiOuput.Count);
+                    //for (int i = 0; i < kanjiOuput.Count; i++) { 
+                    //    Debug.Log(kanjiOuput[i].ToString());
+                    //}
+
+                    Debug.Log(kanjiOuput.Count);
+                    outputSelection.SetButtons(kanjiOuput.Count);
+                    List<string> choices = new List<string>();
+                    for (int i = 0; i < kanjiOuput.Count; i++)
+                    {
+                        choices.Add(kanjiOuput[i]);
+                    }
+                    outputSelection.SetButtonsName(choices);
+                    outputSelection.SetRequestor(this);
+                    outputSelection.TogglePanel(true);
+
+                    while (stringOutput == string.Empty) yield return null;
+                    Debug.Log(stringOutput);
+                }
+                else if (kanjiOuput.Count == 1)
+                {
+                    //Debug.Log("Single kanji output: " + kanjiOuput[0].ToString());
+                    stringOutput = kanjiOuput[0];
+                }
+
+                foreach (char c in stringOutput)
                 {
                     Item.Symbol symbole = new Item.Symbol();
                     symbole.character = c;
@@ -65,5 +100,12 @@ public class Kanjificator : Factory
         output.SetItem(spawnedItem);
 
         state = BuildingState.IDLE;
+    }
+
+    public void ReceiveChoice(string message)
+    {
+        Debug.Log("Selected kanji is: " + message);
+        outputSelection.TogglePanel(false);
+        stringOutput = message;
     }
 }
---
+++ Assets/Scripts/UI/KanjificatorOutputSelection.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KanjificatorOutputSelection : MonoBehaviour
+{
+    [SerializeField] private GameObject kanjiSelectionPanel;
+    [SerializeField] private Button choiceButtonPrefab;
+    [SerializeField] private GridLayoutGroup buttonLayout;
+    [SerializeField] private List<Button> choiceButtonList;
+    [SerializeField] private int numberOfChoice = 2;
+    [SerializeField] private int maxButtonNumber = 5;
+
+    private Kanjificator requestor;
+
+    private void Start()
+    {
+        CreateSelectionButtons();
+    }
+
+    private void CreateSelectionButtons()
+    {
+        for (int i = 0; i < maxButtonNumber; i++)
+        {
+            CreateSelectionButton(i);
+        }
+    }
+
+    private void CreateSelectionButton(int index)
+    {
+        Button button = Instantiate(choiceButtonPrefab);
+        button.transform.SetParent(buttonLayout.transform);
+        button.onClick.AddListener(delegate { SendSelectedOutput(index); });
+
+        button.GetComponentInChildren<TMP_Text>().text = index.ToString();
+        button.gameObject.SetActive(false);
+
+        choiceButtonList.Add(button);
+    }
+
+    #region Setter / Getter
+    public void SetButtons(int numberOfChoice)
+    {
+        this.numberOfChoice = numberOfChoice;
+
+        for (int i = 0; i < numberOfChoice; i++)
+        {
+            choiceButtonList[i].gameObject.SetActive(true);
+        }
+    }
+
+    public void SetButtonsName(List<string> choiceNames)
+    {
+        for (int i = 0; i < numberOfChoice; i++)
+        {
+            choiceButtonList[i].GetComponentInChildren<TMP_Text>().text = choiceNames[i];
+        }
+    }
+
+    public void SetRequestor(Kanjificator req)
+    {
+        this.requestor = req;
+    }
+    #endregion
+
+    public void TogglePanel(bool enabled)
+    {
+        kanjiSelectionPanel.SetActive(enabled);
+    }
+
+    public void SendSelectedOutput(int index)
+    {
+        Debug.Log("Button clicked " +  index);
+        requestor.ReceiveChoice(choiceButtonList[index].GetComponentInChildren<TMP_Text>().text);
+    }
+}
