d341ab41e9a12a68903db386be2a1110949642c5
M2G <traschneider@gmail.com>
2025-02-03 23:10:01 +0100

    Hide goals

--- Assets/Scripts/UI/ShowControls.cs
+++ Assets/Scripts/UI/ShowControls.cs
@@ -12,6 +12,8 @@ public class ShowControls : MonoBehaviour
     [Header("Pin")]
     [SerializeField] private Sprite pinnedSprite;
     [SerializeField] private Sprite notPinnedSprite;
+    [SerializeField] private bool right;
+    [SerializeField] private string id;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,7 @@ public class ShowControls : MonoBehaviour
 
         pinButton = GetComponentInChildren<MultiLayerButton>();
 
-        controlsShown = PlayerPrefs.GetInt("ShowControls", 1) == 1;
+        controlsShown = PlayerPrefs.GetInt("ShowControls" + id, 1) == 1;
 
         if (controlsShown)
         {
@@ -49,7 +51,7 @@ public class ShowControls : MonoBehaviour
     private void Pin()
     {
         controlsShown = true;
-        PlayerPrefs.SetInt("ShowControls", 1);
+        PlayerPrefs.SetInt("ShowControls" + id, 1);
         pinButton.SetIconSprite(pinnedSprite);
         transform.localPosition = basePosition;
     }
@@ -57,8 +59,8 @@ public class ShowControls : MonoBehaviour
     private void Unpin()
     {
         controlsShown = false;
-        PlayerPrefs.SetInt("ShowControls", 0);
+        PlayerPrefs.SetInt("ShowControls" + id, 0);
         pinButton.SetIconSprite(notPinnedSprite);
-        transform.localPosition = new Vector2(-(1920 + rectTransform.sizeDelta.x) / 2, basePosition.y);
+        transform.localPosition = new Vector2((right ? 1 : -1) * (1920 + rectTransform.sizeDelta.x) / 2, basePosition.y);
     }
 }
